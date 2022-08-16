0f9f96c8e8aa655fa051b4f7c3823c7ce658a7cc
Rui Cortijo <ruicortijooliveira@gmail.com>
2022-08-16 22:10:42 +0100

    fix connection string mongoose

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 4000
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017' ,
+mongoose.connect('mongodb://localhost:27017/Languagele' ,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
