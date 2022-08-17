7eb1508d6b8722bf8bbae73a625e5d0c27a3d848
Rui Cortijo <ruicortijooliveira@gmail.com>
2022-08-17 12:03:33 +0100

    front view initial setup

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
-const app = express()
+const app = new express()
+const path = require('path')
+
 const port = 4000
 const mongoose = require('mongoose');
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+app.use('/umd', express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 
 mongoose.connect('mongodb://localhost:27017/Languagele' ,
   {
@@ -10,10 +20,8 @@ mongoose.connect('mongodb://localhost:27017/Languagele' ,
   }
 );
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+const mainController = require('./controllers/main');
+app.get('/', mainController);
 
 
 app.listen(port, () => {
